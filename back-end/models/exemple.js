32626c6776ad14eedb7cf9dd917e7804f1db84d4
theophile.renouf <theophile.renouf@viacesi.fr>
2021-11-17 09:07:41 +0100

    Init

--- back-end/models/exemple.js
+++ back-end/models/exemple.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const testSchema = mongoose.Schema({
   text: { type: String, required: true },
   number: { type: Number, required: true },
+  sessionid: {type: String, required: true},
+  timestamp: {type: Number, required: true},
 });
 
 module.exports = mongoose.model('Test', clickSchema);
